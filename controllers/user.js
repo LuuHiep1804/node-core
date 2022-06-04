8e2d28d945d8cdbdde957bd6d70710aaf5656449
LuuHiep1804 <luuhiep1804@gmail.com>
2022-06-04 17:40:09 +0700

    feature/add user

---
+++ controllers/user.js
@@ -0,0 +1,26 @@
+import { UserModel } from "../models/UserModel";
+
+export const addUser = async (req, res) => {
+    try {
+        const infoUser = req.body;
+        const checkAvailable = await UserModel.find({email: infoUser.email});
+        if(checkAvailable == null) {
+            let user = new UserModel({
+                email: infoUser.email,
+                password: infoUser.password,
+                nickname: infoUser.nickname,
+                phone: infoUser.phone
+            });
+            let saveUser = await user.save();
+            console.log(saveUser);
+            res.status(200).json({
+                success: true,
+                message: "Đã thêm người dùng thành công.",
+                username: user.nickname
+            })
+        }
+    } catch (err) {
+        console.log('err' + err);
+        res.status(500).send(err);
+    }
+}
