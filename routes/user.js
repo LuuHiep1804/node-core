2fc6060bffb8cbc03700582f83f4bd429469e4c6
LuuHiep1804 <luuhiep1804@gmail.com>
2022-06-06 22:53:53 +0700

    api update user

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { addUser, getUser } from "../controllers/user.js";
+import { addUser, getUser, updateUser } from "../controllers/user.js";
 
 const router = express.Router();
 
 router.post('/register', addUser);
 
 router.get('/', getUser);
+
+router.put('/update-user/:userId', updateUser);
 
 export default router;
