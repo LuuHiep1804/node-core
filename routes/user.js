23fce4cf47525d936f3f55ba66a38df0d390e43c
LuuHiep1804 <luuhiep1804@gmail.com>
2022-06-07 23:20:40 +0700

    authorization

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,16 @@
 import express from "express";
+import ROLE from "../config/role.js";
 import { addUser, deleteUser, getUser, updateUser } from "../controllers/user.js";
+import { authRole, verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.post('/register', addUser);
+router.post('/register',verifyToken, authRole(ROLE.ADMIN), addUser);
 
 router.get('/', getUser);
 
-router.put('/update-user/:userId', updateUser);
+router.put('/update-user/:userId', verifyToken, authRole(ROLE.USER), updateUser);
 
-router.delete('/delete-user/:userId', deleteUser);
+router.delete('/delete-user/:userId', verifyToken, authRole(ROLE.ADMIN), deleteUser);
 
 export default router;
