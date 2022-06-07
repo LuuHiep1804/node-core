c370d6580166f6695b1c00495067a9a0d2db0ffa
LuuHiep1804 <luuhiep1804@gmail.com>
2022-06-07 23:18:26 +0700

    generate token

--- helpers/auth.js
+++ helpers/auth.js
@@ -4,7 +4,8 @@ export const generateToken = (user, signature, timeLife) => {
     const userData = {
         _id: user._id,
         email: user.email,
-        nickname: user.nickname
+        nickname: user.nickname,
+        role: user.role
     }
     const token = jwt.sign({data: userData}, signature, {expiresIn: timeLife});
     return token;
